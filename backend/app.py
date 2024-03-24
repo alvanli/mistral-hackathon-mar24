8529693e519a5921d93722e451b85bf780946138
Anthony Susevski <asusevski@gmail.com>
2024-03-24 03:52:15 -0400

    add query

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from collections import defaultdict
 import json
 import numpy as np
+
+import weaviate
+import weaviate.classes as wvc
+import os
+import requests
+import json
+import pandas as pd
+
+from mistralai.client import MistralClient
+import time
+
+
+MISTRAL_API = "TWfVrlX659GSTS9hcsgUcPZ8uNzfoQsg"
+
 
 app = Flask(__name__)
 CORS(app)
@@ -58,5 +72,48 @@ def get_embedding_map():
         
     })
 
+
+@app.route('/query_vector_db', methods=['POST'])
+def process_text():
+    data = request.get_json()
+    text = data.get('text', '')
+    result = main(text)
+    result = [obj.properties['company_name'] for obj in result.objects]
+    return {'result': result}, 200
+
+
+def query_collection(query, collection, mistral_client):
+    ### Embed query
+    emb_query = mistral_client.embeddings(
+        model="mistral-embed",
+        input=[query]
+    ).data[0].embedding
+
+    result = (
+        collection.query.near_vector(
+        near_vector=emb_query, 
+            limit=2
+        )
+    )
+
+    return result
+
+
+def handle_query(query):
+    WCS_CLUSTER_URL = "https://anthony-sandbox-7f0z4seo.weaviate.network"
+    client = weaviate.connect_to_wcs(
+        cluster_url=WCS_CLUSTER_URL,
+        auth_credentials=None
+    )
+
+    mistral_client = MistralClient(api_key=MISTRAL_API)
+    companies = client.collections.get("YCCompanies")
+    news = client.collections.get("News") # what 2 do here?
+    result = query_collection(query, companies, mistral_client)
+
+    return result
+
+
 if __name__=='__main__': 
     app.run(debug=True)
+    
