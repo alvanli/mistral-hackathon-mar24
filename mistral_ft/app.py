d9d169f29ac9657aa085fd68c20f653058c969d2
Alvin on Sharkbyte <sharkbyte@uwdatascience.ca>
2024-03-24 12:50:21 -0400

    added local inference

--- mistral_ft/app.py
+++ mistral_ft/app.py
@@ -4,72 +4,66 @@ from flask_cors import CORS
 from collections import defaultdict
 import json
 import numpy as np
-
-import weaviate
-import weaviate.classes as wvc
 import os
 import requests
 import json
 import pandas as pd
 
-from mistralai.client import MistralClient
-import time
-
-
-MISTRAL_API = "TWfVrlX659GSTS9hcsgUcPZ8uNzfoQsg"
-
+import torch
+from transformers import (
+    BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
+)
 
 app = Flask(__name__)
 CORS(app)
 
-BASE_PATH = "./last_1000"
+BASE_PATH = "/exp/mistral_instruct_generation/checkpoint-1000"
 
-@app.route("/get_embedding_map", methods = ['GET'])
+tokenizer = AutoTokenizer.from_pretrained(BASE_PATH)
+
+nf4_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    BASE_PATH,
+    device_map='auto',
+    load_in_4bit=True, low_cpu_mem_usage=True, 
+    # quantization_config=nf4_config,
+    use_cache=False,
+    attn_implementation="flash_attention_2",
+    max_length=1000
+)
+
+
+# tokenizer_gm = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+# model_gm = AutoModelForCausalLM.from_pretrained(
+#     "mistralai/Mistral-7B-Instruct-v0.2",
+#     quantization_config=nf4_config,
+#     device_map='auto',
+#     attn_implementation="sdpa",
+#     max_length=1000
+# )
+
+# model_gm = model_gm.eval()
+model = model.eval()
+
+@app.route("/chat", methods = ['POST'])
 def get_embedding_map():
-    with open(f"{BASE_PATH}/projections.npy", "rb") as f:
-        projections = np.load(f).tolist()
+    data = request.get_json()
+    text = data.get('text', '')
+    with torch.no_grad():
+        model_input = tokenizer.encode(text, return_tensors="pt").to('cuda')
+        
+        result = model.generate(input_ids=model_input, use_cache=True)
+        # result = model.generate(input_ids=model_input, use_cache=True, assistant_model=model_gm)
 
-    with open(f"{BASE_PATH}/cluster_labels.npy", "rb") as f:
-        cluster_labels = np.load(f).tolist()
-
-    with open(f"{BASE_PATH}/texts.json", "r") as f:
-        texts = json.load(f)
-
-    with open(f"{BASE_PATH}/titles.json", "r") as f:
-        titles = json.load(f)
-    
-    with open(f"{BASE_PATH}/cluster_summaries.json", "r") as f:
-        cluster_summaries = json.load(f)
-    keys = list(cluster_summaries.keys())
-    for key in keys:
-        cluster_summaries[int(key)] = cluster_summaries.pop(key)
-
-    id2cluster = {
-        index: label for index, label in enumerate(cluster_labels)
-    }
-    label2docs = defaultdict(list)
-    for i, label in enumerate(cluster_labels):
-        label2docs[label].append(i)
-    cluster_centers = {}
-    for label in label2docs.keys():
-        x = np.mean([projections[doc][0] for doc in label2docs[label]])
-        y = np.mean([projections[doc][1] for doc in label2docs[label]])
-        cluster_centers[label] = (x, y)
-
-    return jsonify({
-        "companies": [{
-            "projection": proj,
-            "label": lab,
-            "text": text,
-            "title": title
-        } for proj, lab, text, title in zip(projections, cluster_labels, texts, titles)],
-        "clusters": {
-            k: {
-                "center": cluster_centers[k],
-                "summary": cluster_summaries[k]
-            } for k in cluster_centers.keys()
-        }
-    })
+        generated_text = tokenizer.decode(result[0], skip_special_tokens=True)
+        print(generated_text)
+    return jsonify({'output': generated_text})
 
 if __name__=='__main__': 
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5555)
